4110c3b8860975541902fa2bed0952ccb0736970
xSkullxNinja <tarndt@student.neumont.edu>
2016-08-16 10:19:57 -0600

    Fixing Keyboard controls to fire once when key is initially pressed and constantly fire while the key is pressed.

--- Assets/JavaScript/Controls/Keyboard.js
+++ Assets/JavaScript/Controls/Keyboard.js
@@ -2,34 +2,62 @@ var keyFunctions = [];
 
 function handleKeyDown(evt) {
 	if (keyFunctions[evt.keyCode] !== undefined && keyFunctions[evt.keyCode] !== null) {
-		for(var keyFuncNum = 0; keyFuncNum < keyFunctions[evt.keyCode].down.length; ++keyFuncNum) {
-			keyFunctions[evt.keyCode].down[keyFuncNum]();
+		if(!keyFunctions[evt.keyCode].value) {
+			for(var keyFuncNum = 0; keyFuncNum < keyFunctions[evt.keyCode].down.length; ++keyFuncNum) {
+				keyFunctions[evt.keyCode].down[keyFuncNum]();
+			}
+			keyFunctions[evt.keyCode].value = true;
 		}
 	}
 }
 function assignKeyDownFunction(keyCode, keyDownFunction) {
 	if(keyFunctions[keyCode] === undefined || keyFunctions[keyCode] === null) {
-		keyFunctions[keyCode] = { down: [], up: [] };
+		keyFunctions[keyCode] = { value: false, down: [], up: [], press: [] };
 	}
 	var pos = keyFunctions[keyCode].down.length;
 	keyFunctions[keyCode].down[pos] = keyDownFunction;
 }
-function clearKeyDownFunctions(keyCode) {
-	keyFunctions[keyCode].down = [];
+function handleKeyPress(evt) {
+	if (keyFunctions !== undefined && keyFunctions !== null) {
+		for(var keyNum = 0; keyNum < keyFunctions.length; ++keyNum) {
+			if(keyFunctions[keyNum].value) {
+				for(var keyFuncNum = 0; keyFuncNum < keyFunctions[keyNum].press.length; ++keyFuncNum) {
+					keyFunctions[keyNum].press[keyFuncNum]();
+				}
+			}
+		}
+	}
+}
+function assignKeyPressFunction(keyCode, keyPressFunction) {
+	if(keyFunctions[keyCode] === undefined || keyFunctions[keyCode] === null) {
+		keyFunctions[keyCode] = { value: false, down: [], up: [], press: [] };
+	}
+	var pos = keyFunctions[keyCode].press.length;
+	keyFunctions[keyCode].press[pos] = keyPressFunction;
 }
 function handleKeyUp(evt) {
 	if (keyFunctions[evt.keyCode] !== undefined && keyFunctions[evt.keyCode] !== null) {
-		for(var keyFuncNum = 0; keyFuncNum < keyFunctions[evt.keyCode].up.length; ++keyFuncNum) {
-			keyFunctions[evt.keyCode].up[keyFuncNum]();
+		if(keyFunctions[evt.keyCode].value) {
+			for(var keyFuncNum = 0; keyFuncNum < keyFunctions[evt.keyCode].up.length; ++keyFuncNum) {
+				keyFunctions[evt.keyCode].up[keyFuncNum]();
+			}
+			keyFunctions[evt.keyCode].value = false;
 		}
 	}
 }
 function assignKeyUpFunction(keyCode, keyUpFunction) {
 	if(keyFunctions[keyCode] === undefined || keyFunctions[keyCode] === null) {
-		keyFunctions[keyCode] = { down: [], up: [] };
+		keyFunctions[keyCode] = { value: false, down: [], up: [], press: [] };
 	}
 	var pos = keyFunctions[keyCode].up.length;
 	keyFunctions[keyCode].up[pos] = keyUpFunction;
+}
+
+function clearKeyDownFunctions(keyCode) {
+	keyFunctions[keyCode].down = [];
+}
+function clearKeyPressFunctions(keyCode) {
+	keyFunctions[keyCode].press = [];
 }
 function clearKeyUpFunctions(keyCode) {
 	keyFunctions[keyCode].up = [];
